19eb3e75f7d959226d1b01c3df720cc890bcf54d
SashaOK47 <hvorost22@mail.ru>
2021-09-26 19:21:51 +0300

    CT-5: - did hide / show block

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     const burger = document.querySelector('.header__burger'),
-          menu = document.querySelector('.header__menu');
+          menu = document.querySelector('.header__menu'),
+          additionalTrigger = document.querySelector('.additional-services__button'),
+          additionalBlock = document.querySelector('.additional-services__list');
+          
+    additionalTrigger.addEventListener('click', () => {
+        additionalBlock.classList.toggle('additional-services__list--active');
+        additionalTrigger.classList.toggle('js-rotate');
+    })
+
+
     burger.addEventListener('click', () => {
         burger.classList.toggle('active');
         menu.classList.toggle('active');
     });
+
 });
